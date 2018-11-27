497792f7dc29cde39b3d0e30324126f8639dfd21
lucasexe <lucasrangelrm@gmail.com>
2018-11-27 19:15:24 -0200

    Update fixed_size_ptr.h

--- fixed_size_ptr.h
+++ fixed_size_ptr.h
@@ -3,11 +3,11 @@
 
 template<class T>
 struct fixed_size_ptr{
-    uint64_t data;
+	uint64_t data;
 
 	fixed_size_ptr<T>(const T* pos = nullptr) :
-        data((uint64_t)pos)
-    {}
+		data((uint64_t)pos)
+	{}
 
 	T& operator[](const size_t k){
 		return ((T*)data)[k];
@@ -17,7 +17,7 @@ struct fixed_size_ptr{
 		return (T*)data;
 	}
 
-    T& operator*(){
+	T& operator*(){
 		return *(T*)data;
 	}
 	
@@ -25,42 +25,42 @@ struct fixed_size_ptr{
 		return (T*)data == other;
 	}
 
-    fixed_size_ptr<T>& operator+=(const size_t k){
-        data += k * sizeof(T);
+	fixed_size_ptr<T>& operator+=(const size_t k){
+		data += k * sizeof(T);
 		return *this;
 	}
 
-    fixed_size_ptr<T>& operator-=(const size_t k){
+	fixed_size_ptr<T>& operator-=(const size_t k){
 		data -= k * sizeof(T);
-        return *this;
+		return *this;
 	}
 
-    fixed_size_ptr<T>& operator++(){
+	fixed_size_ptr<T>& operator++(){
 		data += sizeof(T);
-        return *this;
+		return *this;
 	}
 
-    fixed_size_ptr<T> operator++(int){
-        fixed_size_ptr<T> aux = (T*)data;
-        data += sizeof(T);
+	fixed_size_ptr<T> operator++(int){
+		fixed_size_ptr<T> aux = (T*)data;
+		data += sizeof(T);
 		return aux;
 	}
 
-    fixed_size_ptr<T>& operator--(){
-        data -= sizeof(T);
+	fixed_size_ptr<T>& operator--(){
+		data -= sizeof(T);
 		return *this;
 	}
-    fixed_size_ptr<T> operator--(int){
-        fixed_size_ptr<T> aux = (T*)data;
-        data -= sizeof(T);
+	fixed_size_ptr<T> operator--(int){
+		fixed_size_ptr<T> aux = (T*)data;
+		data -= sizeof(T);
 		return (T*)aux;
 	}
 
-    fixed_size_ptr<T> operator+(const size_t k){
-        return (T*)(data + k * sizeof(T));
+	fixed_size_ptr<T> operator+(const size_t k){
+		return (T*)(data + k * sizeof(T));
 	}
 
-    fixed_size_ptr<T> operator-(const size_t k){
-        return (T*)(data - k * sizeof(T));
+	fixed_size_ptr<T> operator-(const size_t k){
+		return (T*)(data - k * sizeof(T));
 	}
 };
